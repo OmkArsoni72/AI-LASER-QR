4aae34874c75dee5df96aa4c22c59ccd62d4d7e6
Tushar Kumar <tushar76540@gmail.com>
2025-09-29 00:58:06 +0530

    solve error

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,17 @@ export default defineConfig(({ mode }) => ({
   },
     build: {
       chunkSizeWarningLimit: 2000, // Increase limit to 2000 KB
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('react')) return 'vendor-react';
+              if (id.includes('@mui')) return 'vendor-mui';
+              if (id.includes('chart.js')) return 'vendor-chartjs';
+              return 'vendor';
+            }
+          },
+        },
+      },
     },
   }));
